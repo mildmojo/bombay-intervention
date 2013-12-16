c5feb840c037e0cb031db5d7d6dc742a513ddc24
mildmojo <theaterfire@yahoo.com>
2013-12-16 02:15:04 -0500

    Fixes X/Y swapped bug in Screen2D.js.

--- Assets/scripts/Screen2D.js
+++ Assets/scripts/Screen2D.js
@@ -3,6 +3,6 @@ class Screen2D {
     return Camera.main.ViewportToWorldPoint(Vector3(1,0,0)).x;
   }
   static function worldHeight() : float {
-    return Camera.main.ViewportToWorldPoint(Vector3(1,0,0)).y;
+    return Camera.main.ViewportToWorldPoint(Vector3(0,1,0)).y;
   }
 }
