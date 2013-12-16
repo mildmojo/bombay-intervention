8b3b3829b866cbae22ecb5dedb494dbbc0f8e973
mildmojo <theaterfire@yahoo.com>
2013-12-16 02:12:55 -0500

    Adds TextMesh variable anchor support to snap behaviors.

--- Assets/scripts/SnapToRight.js
+++ Assets/scripts/SnapToRight.js
@@ -1,12 +1,31 @@
 #pragma strict
 
+// Zero to one
+var paddingPercent : float = 0;
+
 function Start() {
-  LockToViewportTop();
+  SnapToViewportRight();
 }
 
-function LockToViewportTop() {
+function SnapToViewportRight() {
   var frame = new TwoDee(gameObject);
-  var x = Screen2D.worldWidth() - frame.width;
+  var x = Screen2D.worldWidth() - frame.width * paddingPercent; // works for right-anchored objects
+  var textMesh = transform.GetComponent.<TextMesh>();
+  if (textMesh) {
+    switch(textMesh.anchor) {
+      case TextAnchor.UpperCenter:
+      case TextAnchor.MiddleCenter:
+      case TextAnchor.LowerCenter:
+        x -= frame.width / 2.0;
+        break;
+      case TextAnchor.UpperLeft:
+      case TextAnchor.MiddleLeft:
+      case TextAnchor.LowerLeft:
+        x -= frame.width;
+        break;
+    }
+  }
+Debug.Log('snapping X to ' + x);
   var oldPos = frame.position;
   transform.position = Vector3(x, oldPos.y, oldPos.z);
 }
--- Assets/scripts/SnapToTop.js
+++ Assets/scripts/SnapToTop.js
@@ -1,11 +1,31 @@
 #pragma strict
 
+// Zero to one
+var paddingPercent : float = 0;
+
 function Start() {
-  LockToViewportTop();
+  SnapToViewportTop();
 }
 
-function LockToViewportTop() {
-  var y = Screen2D.worldHeight();
+function SnapToViewportTop() {
+  var frame = new TwoDee(gameObject);
+  var y = Screen2D.worldHeight() - frame.height * paddingPercent; // works for top-anchored objects
+  var textMesh = transform.GetComponent.<TextMesh>();
+  if (textMesh) {
+    switch(textMesh.anchor) {
+      case TextAnchor.MiddleLeft:
+      case TextAnchor.MiddleCenter:
+      case TextAnchor.MiddleRight:
+        y -= frame.height / 2.0;
+        break;
+      case TextAnchor.LowerLeft:
+      case TextAnchor.LowerCenter:
+      case TextAnchor.LowerRight:
+        y -= frame.height;
+        break;
+    }
+  }
+Debug.Log('snapping Y to ' + y);
   var oldPos = transform.position;
   transform.position = Vector3(oldPos.x, y, oldPos.z);
 }
