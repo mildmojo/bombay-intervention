91755ad801e894a7e382125c850272cb42c930bf
mildmojo <theaterfire@yahoo.com>
2014-01-01 16:54:34 -0500

    Adds Screen2D.worldCenter() and Screen2D.worldCenterAtZ().

--- Assets/scripts/Screen2D.js
+++ Assets/scripts/Screen2D.js
@@ -5,4 +5,13 @@ class Screen2D {
   static function worldHeight() : float {
     return Camera.main.ViewportToWorldPoint(Vector3(0,1,0)).y;
   }
+  static function worldCenter() {
+    return worldCenterAtZ(0);
+  }
+  static function worldCenterAtZ(gameObject : GameObject) {
+    return worldCenterAtZ(gameObject.transform.position.z);
+  }
+  static function worldCenterAtZ(z) {
+    return Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, z);
+  }
 }
